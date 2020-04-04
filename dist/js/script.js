3452c414e8cddb00b3810b9b92eb212f445ab6e2
mohammad-alfatih <alfaitha@gmail.com>
2020-04-03 22:56:53 -0400

    use XHMHttpRequest instead of jquery ajax

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,13 +3,24 @@ $(document).ready(function() {
     $("#submitQuoteRequest").click(function(e) {
         var formData = $("#contact-form").serialize();
         console.log(formData);
-        $.ajax({
-            url: "",
-            type: "POST",
-            success: function(e) {
-                console.log(e);
-            }
-        });
+
+        // $.ajax({
+        //     url: "http://localhost:3000/contact",
+        //     type: "POST",
+        //     success: function(e) {
+        //         console.log(e);
+        //     }
+        // });
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            // if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            // }
+        };
+        xhttp.open("POST", "http://localhost:3000/contact", true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.send("fname=Henry&lname=Ford");
     });
     
 });
