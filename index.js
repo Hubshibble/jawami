d04e4eff5c08e4f2bd47b4e19c5813cd62c43151
mohammad-alfatih <alfaitha@gmail.com>
2020-04-10 16:36:06 -0400

    connect to local sql DB via node application

--- index.js
+++ index.js
@@ -6,17 +6,22 @@ const request = require('request')
 const https = require('https');
 
 const PORT = process.env.PORT || 5000;
-const mailChimpApiCred = {
-  user: "xxx",
-  key: "xxx",
-  listId: "xxx",
-  dataCenter: "xxx",
-  url: ""
-}
+const mysqlConnection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "xxx",
+  database: "xxx",
+  port: 3306,
+  multipleStatements: true
+});
+mysqlConnection.connect((err) => {
+  if(!err) console.log("MySQL connection created successfully");
+  else console.log("MySQL connection failed\n", err);
+});
 
-express()
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }))
+const app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
@@ -24,62 +29,8 @@ express()
   .post('/contact', (req, res) => {
     const contactData = req.body;
 
-    const endpoint = 'https://' + mailChimpApiCred.dataCenter + '.api.mailchimp.com/3.0/lists/' + mailChimpApiCred.listId + '/members/';
-    const base64encodedData = new Buffer(mailChimpApiCred.user + ':' + mailChimpApiCred.key).toString('base64');
-    const dataBody = {
-      status: "subscribed",
-      emailAddress: contactData.email,
-      merge_fields: {
-        FNAME: contactData.firstName,
-        LNAME: contactData.lastName,
-        PHONE: contactData.phone
-      },
-      interests: {
-        title: contactData.need
-      }
-    };
-
-    const dataString =  JSON.stringify(dataBody);
-    const options = {
-      hostname: endpoint,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': dataString.length,
-        'Authorization': 'Basic ' + base64encodedData
-      }
-    };
-    
-    console.log('Making a request with this data:', dataString);
-    console.log('Making a request with these options:', options);
-
-    const httpsReq = https.request(options, httpsRes => {
-      console.log(`statusCode: ${httpsRes.statusCode}`);
-    
-      let body = '';
-
-      httpsRes.on('data', d => {
-        console.log('Res return value:', d);
-        
-        body += d;
-        // process.stdout.write(d);
-      });
-
-      httpsRes.on('end', () => {
-        console.log('Body of data:', body.length);
-        httpsRes.statusCode != 200 ? console.log('API call failed:', httpsRes.statusCode) : console.log('API call successful:', httpsRes.statusCode);
-      });
-    });
-
-    httpsReq.on('error', error => {
-      console.error(error);
-    });
-    
-    httpsReq.write(dataString);
-    httpsReq.end();
-
     res.send(JSON.stringify({
-      data_body: req.body
+      data_body: contactData
     }));
 
     console.log('you posted:', req.body);
