be0cd49a76f73571ff3c56554043bb25ef5fee74
mohammad-alfatih <alfaitha@gmail.com>
2020-04-19 01:36:45 -0400

    make insert statements into db from input form

--- routes/contact.js
+++ routes/contact.js
@@ -21,19 +21,21 @@ Router.post('/', (req, res) => {
     const companyData = { NAME: body.company, FIELD: body.industry };
     const requestData = { NEED: body.need, CASH: body.budget, MORE: body.details };
     const user = "Web";
-    const newContactId = dbConnection.query(`INSERT INTO JawamiDB.Contact (FirstName, LastName, Email, Phone, CreatedBy, ModifiedBy) VALUES ("${contactData.FNAME}", "${contactData.LNAME}", "${contactData.EMAIL}", "${contactData.PHONE}", "${user}", "${user}"); SELECT LAST_INSERT_ID();`,
-        (err, rows, fields) => !err ? res.send(rows) : console.log(err)
-    );
-    const newCompanyId = dbConnection.query(`INSERT INTO JawamiDB.Company (ContactId, Name, Industry, CreatedBy, ModifiedBy) VALUES ("${newContactId}", "${companyData.NAME}", "${companyData.FIELD}", "${user}", "${user}"); SELECT LAST_INSERT_ID();`,
-        (err, rows, fields) => !err ? res.send(rows) : console.log(err)
-    );
-    const newInquiryId = dbConnection.query(`INSERT INTO JawamiDB.Inquiry (ContactId, CompanyId, Service, Budget, Details, CreatedBy, ModifiedBy) VALUES ("${newContactId}", "${newCompanyId}", "${requestData.NEED}", "${requestData.CASH}", "${requestData.MORE}", "${user}", "${user}"); SELECT LAST_INSERT_ID();`,
-        (err, rows, fields) => !err ? res.send(rows) : console.log(err)
-    );
+    const result = new Promise((resolve, reject) => {
+        dbConnection.query(`INSERT INTO JawamiDB.Contact (FirstName, LastName, Email, Phone, CreatedBy, ModifiedBy) VALUES ("${contactData.FNAME}", "${contactData.LNAME}", "${contactData.EMAIL}", "${contactData.PHONE}", "${user}", "${user}"); SELECT LAST_INSERT_ID() AS ContactId;`,
+            (err, rows, fields) => (!err) ? resolve(rows[1][0].ContactId) : reject(err)
+        );
+    }).then(newContactId => new Promise((resolve, reject) => {
+        dbConnection.query(`INSERT INTO JawamiDB.Company (ContactId, Name, Industry, CreatedBy, ModifiedBy) VALUES ("${newContactId}", "${companyData.NAME}", "${companyData.FIELD}", "${user}", "${user}"); SELECT LAST_INSERT_ID() AS CompanyId;`,
+            (err, rows, fields) => (!err) ? resolve(rows[1][0].CompanyId) : reject(err)
+        );
+    }).then(newCompanyId => new Promise((resolve, reject) => {
+        dbConnection.query(`INSERT INTO JawamiDB.Inquiry (ContactId, CompanyId, Service, Budget, Details, CreatedBy, ModifiedBy) VALUES ("${newContactId}", "${newCompanyId}", "${requestData.NEED}", "${requestData.CASH}", "${requestData.MORE}", "${user}", "${user}"); SELECT LAST_INSERT_ID() AS InquiryId;`,
+            (err, rows, fields) => (!err) ? resolve(rows) : reject(err)
+        );
+    }).then(newInquiryId => newInquiryId)));
 
-    console.log('new Contact Id:', newContactId);
-    console.log('new Company Id:', newCompanyId);
-    console.log('new Inquiry Id:', newInquiryId);
+    result.then(x => console.log(x));
 });
 
 module.exports = Router;
