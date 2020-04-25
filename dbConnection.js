be14b0fff98a56a3908b09c8757f3e3043140e7c
mohammad-alfatih <alfaitha@gmail.com>
2020-04-25 17:29:33 -0400

    fix db name

--- dbConnection.js
+++ dbConnection.js
@@ -4,7 +4,7 @@ const mysqlConnection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "root",
-    database: "jawamiDB",
+    database: "JawamiDB",
     port: 3306,
     multipleStatements: true
 });
